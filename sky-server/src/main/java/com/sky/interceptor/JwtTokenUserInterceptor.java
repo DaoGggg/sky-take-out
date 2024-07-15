ccb194145c4353e630e8f753b18a1e5a21535d03
xiaotian <1440784616@qq.com>
2024-07-15 21:26:51 +0800

    添加购物车

--- sky-server/src/main/java/com/sky/interceptor/JwtTokenUserInterceptor.java
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenUserInterceptor.java
@@ -34,11 +34,11 @@ public class JwtTokenUserInterceptor implements HandlerInterceptor {
      * @throws Exception
      */
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //判断当前拦截到的是Controller的方法还是其他资源
-        if (!(handler instanceof HandlerMethod)) {
-            //当前拦截到的不是动态方法，直接放行
-            return true;
-        }
+//        //判断当前拦截到的是Controller的方法还是其他资源
+//        if (!(handler instanceof HandlerMethod)) {
+//            //当前拦截到的不是动态方法，直接放行
+//            return true;
+//        }
 
         //1、从请求头中获取令牌
         String token = request.getHeader(jwtProperties.getUserTokenName());
@@ -47,7 +47,7 @@ public class JwtTokenUserInterceptor implements HandlerInterceptor {
         try {
             log.info("jwt校验:{}", token);
             Claims claims = JwtUtil.parseJWT(jwtProperties.getUserSecretKey(), token);
-            Long userId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());
+            Long userId = Long.valueOf(claims.get(JwtClaimsConstant.USER_ID).toString());
             BaseContext.setCurrentId(userId);//存储线程变量（当前用户的员工id）
             log.info("当前用户id：", userId);
             //3、通过，放行
